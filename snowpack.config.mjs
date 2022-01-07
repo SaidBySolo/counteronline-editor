7575e92d0e7898d2b7b500d43f1a3d4a280d8099
Ryu juheon <saidbysolo@gmail.com>
2022-01-07 13:28:14 +0900

    BREAKING CHANGE: migration to typescript

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -4,14 +4,24 @@ export default {
     public: { url: '/', static: true },
     src: { url: '/dist' },
   },
-  plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-dotenv'],
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv',
+    [
+      '@snowpack/plugin-typescript',
+      {
+        /* Yarn PnP workaround: see https://www.npmjs.com/package/@snowpack/plugin-typescript */
+        ...(process.versions.pnp ? { tsc: 'yarn pnpify tsc' } : {}),
+      },
+    ],
+  ],
   routes: [
     /* Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
   ],
   optimize: {
     /* Example: Bundle your final build: */
-    "bundle": true,
+    // "bundle": true,
   },
   packageOptions: {
     /* ... */
