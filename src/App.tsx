3123b64af22388490099214aad02d7828a79025e
Ryu juheon <saidbysolo@gmail.com>
2022-05-17 23:58:31 +0900

    feat: add renderer

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { Center, Divider, Flex } from '@chakra-ui/react';
 import * as React from 'react';
 import { MainAccordion } from './Builder';
+import { PatchNoteBuilder } from './PatchNoteBuilder';
+import ReactDOMServer from 'react-dom/server'
 
 function App() {
+  const [element, setElement] = React.useState<JSX.Element[]>([]);
+  const builder = new PatchNoteBuilder(setElement)
+
   return (
     <Flex>
-      <MainAccordion />
-      <Center height='500px'>
+      <MainAccordion builder={builder} />
+      <Center height='100vh'>
         <Divider orientation='vertical' />
       </Center>
-      <div>
-        here is render
-      </div>
+      <div dangerouslySetInnerHTML={{ __html: element.map(ReactDOMServer.renderToStaticMarkup).join("\n") }} />
     </Flex>
 
   );
