cf71f54ceb81bea2593e33eca6ada349cd7a3da3
Ryu juheon <saidbysolo@gmail.com>
2022-05-17 23:57:46 +0900

    feat: add builder

--- src/PatchNoteBuilder.tsx
+++ src/PatchNoteBuilder.tsx
@@ -1,32 +1,102 @@
+// https://stackoverflow.com/a/3890175
+function linkify(inputText: string) {
+    //URLs starting with http://, https://, or ftp://
+    const replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+    const execed = replacePattern1.exec(inputText)
+    return execed ? <a href={execed[0]}>{execed[0]}</a> : inputText
+}
+
 export class PatchNoteBuilder {
-    constructor() {
+    state: (element: JSX.Element) => void
+    constructor(state: React.Dispatch<React.SetStateAction<JSX.Element[]>>) {
+        this.state = (element: JSX.Element) => { state(prevElements => [...prevElements, element]) }
     }
 
-    addMetadata() {
+    addMetadata(content: string) {
+        this.state(
+            <>
+                <meta property="og:description" content={`이 게시글은 CounterOnline changelog ${content} 패치에 관한 내용을 다루고 있습니다.`} />
+                <p>&nbsp;</p>
 
+            </>
+        )
     }
 
     addLineBreak() {
-
+        this.state(<p>&nbsp;</p>)
     }
 
-    addTitle(content: string) {
+    addTitle(content: string, version: string) {
+        this.state(
+            <>
+                <p>
+                    <span style={{ fontSize: "28px" }}>
+                        <strong>
+                            &nbsp; {`${content} ${version} 업데이트`}
+                        </strong>
+                    </span>
+                </p>
+                <h5>
+                    <span style={{ fontSize: "20px", color: "#999999" }}>
+                        <strong>
+                            {/* @ts-ignore*/}
+                            <strike>
+                                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                                {/* @ts-ignore*/}
+                            </strike>
+                        </strong>
+                    </span>
+                </h5>
+            </>
 
+        )
     }
 
     addCategory(content: string) {
-
+        this.state(<h5 style={{ fontWeight: 800, fontSize: "22px" }}>&nbsp; &nbsp; {content}</h5>)
     }
 
-    addContent(type: 'plus' | 'minus' | 'check', color: string, content: string) {
+    addContent(type: string, color: string, content: string) {
+        this.state(
+            < p >
+                <span style={{ fontSize: "16px", color: color }} >
+                    {/* @ts-ignore*/}
+                    <span class={`fas fa-${type}`}>
+                        &nbsp;
+                    </span>
+                    &nbsp; {content}
+                </span>
+            </p >
+        )
+    }
 
+    addDescription(content: string) {
+        this.state(
+            <p>
+                <span style={{ fontSize: "13px" }}>
+                    &nbsp; &nbsp; &nbsp; &nbsp; {linkify(content)}
+                </span>
+            </p>
+        )
     }
 
     addImage(alt: string, src: string) {
-
+        this.state(
+            < p >
+                <img alt={alt} src={src} style={{ marginTop: "3px", borderRadius: "15px" }} width="70%" />
+            </p >
+        )
     }
 
-    addBlockcontent() {
-
+    addBlockcontent(content: string) {
+        this.state(
+            < blockquote >
+                <p>
+                    <span style={{ fontSize: "16px" }}>
+                        {content}
+                    </span>
+                </p>
+            </blockquote >
+        )
     }
 }
