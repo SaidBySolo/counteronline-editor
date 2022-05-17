9a8aa16c5f8cc1a9845c02b697279327a415330f
Ryu juheon <saidbysolo@gmail.com>
2022-05-17 23:58:11 +0900

    feat: add some accordion

--- src/Builder.tsx
+++ src/Builder.tsx
@@ -1,41 +1,203 @@
-import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box } from '@chakra-ui/react';
+import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Button, Input, Textarea } from '@chakra-ui/react';
 import * as React from 'react';
+import { PatchNoteBuilder } from './PatchNoteBuilder';
 
-
-
-export const MainAccordion = () => {
+export const MainAccordion = ({ builder }: { builder: PatchNoteBuilder }) => {
     return (
-        <Accordion allowToggle>
-            <MetadataAccordion />
+        <Accordion allowToggle width="500px">
+            <MetadataAccordion builder={builder} />
+            <LineBreakAccordion builder={builder} />
+            <TitleAccordion builder={builder} />
+            <CategoryAccordion builder={builder} />
+            <ContentAccordion builder={builder} />
+            <DescriptionAccordion builder={builder} />
+            <ImageAccordion builder={builder} />
+            <BlockcontentAccordion builder={builder} />
         </Accordion>
     )
 }
 
-const AccordionItemBase = ({ name }: { name: string }) => {
-    return (
-        <h2>
-            <AccordionButton>
-                <Box flex='1' textAlign='left'>
-                    {name}
-                </Box>
-                <AccordionIcon />
-            </AccordionButton>
-        </h2>
-    )
-
-}
-
-const MetadataAccordion = () => {
+const AccordionItemBase = ({ name, children }: { name: string, children: React.ReactNode }) => {
     return (
         <AccordionItem>
-            <AccordionItemBase name="Add Metadata" />
+            <h2>
+                <AccordionButton>
+                    <Box flex='1' textAlign='left'>
+                        {name}
+                    </Box>
+                    <AccordionIcon />
+                </AccordionButton>
+            </h2>
             <AccordionPanel pb={4}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat.
+                {children}
             </AccordionPanel>
         </AccordionItem>
+    )
 
+
+}
+const LineBreakAccordion = ({ builder }: { builder: PatchNoteBuilder }) => {
+    return (
+        <AccordionItemBase name="Add LineBreak">
+            <Button onClick={() => builder.addLineBreak()}>
+                Add
+            </Button>
+        </AccordionItemBase>
+
+    )
+}
+
+const MetadataAccordion = ({ builder }: { builder: PatchNoteBuilder }) => {
+    const [contentValue, setContentValue] = React.useState('')
+    const handleContentChange = (event: React.ChangeEvent<HTMLInputElement>) => setContentValue(event.target.value)
+
+    return (
+        <AccordionItemBase name="Add Metadata">
+            <Input
+                value={contentValue}
+                onChange={handleContentChange}
+                placeholder='content' />
+            <Button onClick={() => builder.addMetadata(contentValue)}>
+                Add
+            </Button>
+        </AccordionItemBase>
+    )
+}
+
+
+const TitleAccordion = ({ builder }: { builder: PatchNoteBuilder }) => {
+    const [contentValue, setContentValue] = React.useState('')
+    const handleContentChange = (event: React.ChangeEvent<HTMLInputElement>) => setContentValue(event.target.value)
+    const [versionValue, setVersionValue] = React.useState('')
+    const handleVersionChange = (event: React.ChangeEvent<HTMLInputElement>) => setVersionValue(event.target.value)
+    return (
+        <AccordionItemBase name="Add Title">
+            <Input
+                placeholder='content'
+                value={contentValue}
+                onChange={handleContentChange}
+            />
+            <Input
+                placeholder='version'
+                value={versionValue}
+                onChange={handleVersionChange} />
+            <Button onClick={() => {
+                builder.addTitle(contentValue, versionValue)
+            }}>
+                Add
+            </Button>
+        </AccordionItemBase>
+    )
+}
+
+const CategoryAccordion = ({ builder }: { builder: PatchNoteBuilder }) => {
+    const [contentValue, setContentValue] = React.useState('')
+    const handleContentChange = (event: React.ChangeEvent<HTMLInputElement>) => setContentValue(event.target.value)
+    return (
+        <AccordionItemBase name="Add Category">
+            <Input
+                placeholder='content'
+                value={contentValue}
+                onChange={handleContentChange}
+            />
+            <Button onClick={() => {
+                builder.addCategory(contentValue)
+            }}>
+                Add
+            </Button>
+        </AccordionItemBase>
+    )
+}
+
+const ContentAccordion = ({ builder }: { builder: PatchNoteBuilder }) => {
+    const [typeValue, setTypeValue] = React.useState('')
+    const handleTypeChange = (event: React.ChangeEvent<HTMLInputElement>) => setTypeValue(event.target.value)
+    const [colorValue, setColorValue] = React.useState('')
+    const handleColorChange = (event: React.ChangeEvent<HTMLInputElement>) => setColorValue(event.target.value)
+    const [contentValue, setContentValue] = React.useState('')
+    const handleContentChange = (event: React.ChangeEvent<HTMLInputElement>) => setContentValue(event.target.value)
+    return (
+        <AccordionItemBase name="Add Content">
+            <Input
+                placeholder='type: plus | minus | check'
+                value={typeValue}
+                onChange={handleTypeChange}
+            />
+            <Input
+                placeholder='color'
+                value={colorValue}
+                onChange={handleColorChange}
+            />
+            <Input
+                placeholder='content'
+                value={contentValue}
+                onChange={handleContentChange}
+            />
+            <Button onClick={() => {
+                builder.addContent(typeValue, colorValue, contentValue)
+            }}>
+                Add
+            </Button>
+        </AccordionItemBase>
+    )
+}
+
+const DescriptionAccordion = ({ builder }: { builder: PatchNoteBuilder }) => {
+    const [contentValue, setContentValue] = React.useState('')
+    const handleContentChange = (event: React.ChangeEvent<HTMLInputElement>) => setContentValue(event.target.value)
+
+    return (
+        <AccordionItemBase name="Add Description">
+            <Input
+                value={contentValue}
+                onChange={handleContentChange}
+                placeholder='content' />
+            <Button onClick={() => builder.addDescription(contentValue)}>
+                Add
+            </Button>
+        </AccordionItemBase>
+    )
+}
+
+const ImageAccordion = ({ builder }: { builder: PatchNoteBuilder }) => {
+    const [altValue, setAltValue] = React.useState('')
+    const handleAltChange = (event: React.ChangeEvent<HTMLInputElement>) => setAltValue(event.target.value)
+    const [srcValue, setSrcValue] = React.useState('')
+    const handleSrcChange = (event: React.ChangeEvent<HTMLInputElement>) => setSrcValue(event.target.value)
+    return (
+        <AccordionItemBase name="Add Image">
+            <Input
+                placeholder='alt'
+                value={altValue}
+                onChange={handleAltChange}
+            />
+            <Input
+                placeholder='src'
+                value={srcValue}
+                onChange={handleSrcChange}
+            />
+            <Button onClick={() => {
+                builder.addImage(altValue, srcValue)
+            }}>
+                Add
+            </Button>
+        </AccordionItemBase>
+    )
+}
+
+const BlockcontentAccordion = ({ builder }: { builder: PatchNoteBuilder }) => {
+    const [contentValue, setContentValue] = React.useState('')
+    const handleContentChange = (event: React.ChangeEvent<HTMLInputElement>) => setContentValue(event.target.value)
+
+    return (
+        <AccordionItemBase name="Add Blockcontent">
+            <Input
+                value={contentValue}
+                onChange={handleContentChange}
+                placeholder='content' />
+            <Button onClick={() => builder.addBlockcontent(contentValue)}>
+                Add
+            </Button>
+        </AccordionItemBase>
     )
 }
